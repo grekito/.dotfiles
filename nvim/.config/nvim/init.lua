c1a113321c78adc670584b6e5ef26e158865fc3c
grekito <costamarudes@gmail.com>
2025-05-23 10:39:04 -0300

    changes nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,4 @@
--- bread's neovim config
--- keymaps are in lua/config/mappings.lua
--- install a patched font & ensure your terminal supports glyphs
--- enjoy :D
-
--- auto install vim-plug and plugins, if not found
+-- Auto install vim-plug and plugins, if not found
 local data_dir = vim.fn.stdpath('data')
 if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
 	vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
@@ -16,6 +11,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
+Plug ('christoomey/vim-tmux-navigator')
+Plug ('nvim-lua/plenary.nvim')
+
 Plug ('windwp/nvim-autopairs')
 Plug ('nvim-tree/nvim-tree.lua')
 Plug ('nvim-treesitter/nvim-treesitter')
@@ -23,17 +21,24 @@ Plug ('Mofiqul/dracula.nvim')
 Plug ('nvim-lualine/lualine.nvim')
 Plug ('nvim-tree/nvim-web-devicons')
 Plug ('folke/which-key.nvim')
+Plug ('lukas-reineke/indent-blankline.nvim')
+Plug ('mfussenegger/nvim-lint')
+Plug ('numToStr/FTerm.nvim')
 
 vim.call('plug#end')
 
+-- Core configurations
 require('config.mappings')
 require('config.options')
 require('config.theme')
 
-
+-- Plugins configs
 require('plugins.autopairs')
 require('plugins.tree')
 require('plugins.treesitter')
 require('plugins.lualine')
 require('plugins.web-devicons')
 require('plugins.which-key')
+require('plugins.indent-blankline')
+require('plugins.linting')
+require('plugins.fterm')
--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -22,3 +22,7 @@ keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file e
 keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
 keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
 keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+
+-- Fterm
+keymap.set('n', '<leader>z', ":lua require('FTerm').open()<CR>") --open term
+keymap.set('t', '<Esc>', '<C-\\><C-n><CMD>lua require("FTerm").close()<CR>') --preserves session
